2b39e5db4bdbefbd963b9ac161448543296c849c
Suren Oganesian <me@suren.io>
2023-02-05 14:07:23 +0100

    added the accessoryType .checkmark ability to the tableview (add/remove)

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class TodoListViewController: UITableViewController {
     
     let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -27,16 +27,20 @@ class TodoListViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
         cell.textLabel?.text = itemArray[indexPath.row]
-
+        
         return cell
     }
     
     //MARK - TableView Delegate Methods
-
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
-        print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
-    
 }
